7b775855862ab7619fb646794913b0d1d5c0115e
Yuri Victor <yurivictor@gmail.com>
2013-08-29 14:56:52 -0400

    Readded tweet button on hover

--- templates/tweet.php
+++ templates/tweet.php
@@ -2,5 +2,6 @@
     <a href="https://twitter.com/intent/tweet?original_referer=<?php echo urlencode( $permalink); ?>&source=tweetbutton&text=<?php echo rawurlencode( $tweetcontent ); ?>&url=<?php echo urlencode( $permalink ); ?>">
         <?php echo $content; ?>
         <i class="icon-twitter"></i>
+        <span class="share">Tweet</span>
     </a>
 </span>
