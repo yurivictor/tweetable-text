4636d0495e76784848407562aa52b25eef6c231d
Yuri Victor <yurivictor@gmail.com>
2013-08-29 14:43:13 -0400

    init

---
+++ templates/tweet.php
@@ -0,0 +1,6 @@
+<span class="tweetable">
+    <a href="https://twitter.com/intent/tweet?original_referer=<?php echo urlencode( $permalink); ?>&source=tweetbutton&text=<?php echo rawurlencode( $tweetcontent ); ?>&url=<?php echo urlencode( $permalink ); ?>">
+        <?php echo $content; ?>
+        <i class="icon-twitter"></i>
+    </a>
+</span>
--- tweetable-text.php
+++ tweetable-text.php
@@ -1,90 +1,124 @@
 <?php
-
 /*
 Plugin Name: Tweetable Text
 Original Plugin URI: http://wordpress.org/extend/plugins/tweetable-text/
 Description: Make your posts more shareable. Add a Tweet and Buffer button to key sentences right inside each blog post with a simple [tweetable] tag.
 Version: 1.1
-Author: Salim Virani (original), updated by Joshua Benton of Nieman Lab
+Author: Salim Virani (original), updated by Joshua Benton of Nieman Lab, forked by Yuri Victor
 */
 
+if ( ! class_exists ( 'TweetableText' ) ):
 
-//Stops WordPress from converting your quote symbols into smartquotes, since they are not compatible with the Twitter Share button. (The urlencoding of single quotes / apostrophes breaks in the tweet.)
-remove_filter('the_content', 'wptexturize');
+class TweetableText {
 
-class TweetableText{
- function makeTweetable($atts, $content = "") {
-   extract(shortcode_atts(array(
-      'alt' => '',
-      'hashtag' => '',
-   ), $atts));
-   global $wpdb, $post;
+	/** Constants *************************************************************/
 
+	const version          = '0.0.1';
+	const key              = 'tweetable';
 
+	/** Load Methods **********************************************************/
 
-
-	if(get_post_type($post) == "post") {
-		$post_id = $post->ID;
-		$permalink = get_permalink($post_id);
-
-		$tweetcontent = ucfirst(strip_tags($content));
-
-		if ($alt != '') $tweetcontent = $alt;
-		if ($hashtag != '') $tweetcontent .= " " . $hashtag;
-
-		$ret = "<span class='tweetable'>";
-		$ret .= "<a href='https://twitter.com/intent/tweet?original_referer=".urlencode($permalink)."&source=tweetbutton&text=".rawurlencode(($tweetcontent)) ."&url=".urlencode($permalink)."'>$content&thinsp;<i class='icon-twitter' style='color: #ed2e24;'></i>";
-		$ret .= "</a>";
-		$ret .= "<span class='sharebuttons'>";
-
-		$ret .= "<a href='https://twitter.com/intent/tweet?original_referer=".urlencode($permalink)."&source=tweetbutton&text=".rawurlencode(($tweetcontent)) ."&url=".urlencode($permalink)."'>TWEET";
-		$ret .= "</a>";
-
-
-		$ret .= "</span>";
-		$ret .= "</span>";
-            return $ret;
-	} else {
-		return $content;
+	/**
+	 * Load necessary functions
+	 */
+	public static function load() {
+		self::add_actions();
+		self::add_shortcodes();
+		self::remove_filters();
 	}
-     }
+
+	/** 
+	 * Hook actions into WordPress API
+	 * @uses add_action()
+	 */
+	private static function add_actions() {
+		add_action(  'wp_head', array( __CLASS__, 'enqueue_scripts' ) );
+	}
+
+	/**
+	 * Enables shortcodes in WordPress posts
+	 * @uses add_shortcode()
+	 */
+	private static function add_shortcodes() {
+		add_shortcode( 'tweetable', array( __CLASS__, 'makeTweetable' ) );
+	}
+
+	/**
+	 * Remove filters from WordPress API
+	 * @uses remove_filter()
+	 */
+	private static function remove_filters() {
+		// Stops WordPress from converting your quote symbols into smartquotes, since they are not compatible with the Twitter Share button. (The urlencoding of single quotes / apostrophes breaks in the tweet.)
+		remove_filter( 'the_content', 'wptexturize' );
+	}   
+
+	/**
+	 * Enqueue the necessary CSS and JS
+	 * @uses wp_enqueue_style()
+	 */
+	public static function enqueue_scripts() {
+		// css
+		wp_enqueue_style( self::key, plugins_url( 'css/tweetable.css', __FILE__ ), null, self::version );
+	}
+
+	/** Public Methods *****************************************************/
+
+	/**
+	 * Turns [tweetable] shortcode into link
+	 *
+	 * @param array $atts, [tweetable] shortcode attributes
+	 * @param string $content, the content wrapped in [tweetable] shortcode
+	 * @uses shortcode_atts()
+	 * @return if not post
+	 */
+	public static function makeTweetable( $atts, $content = '' ) {
+
+		global $post;
+
+		// bail if not a post
+		if ( ! get_post_type( $post ) == 'post' )
+			return $content;
+
+		// [tweetable] shortcode attributes
+		// @param string alt, an alternate tweet
+		// @param string hashtag, a hashtag to attach to the tweet
+		extract( shortcode_atts( array(
+			'alt'     => '',
+			'hashtag' => '',
+		), $atts ) );
+		$permalink = get_permalink( $post->ID );
+		$tweetcontent = ucfirst( strip_tags( $content ) );
+			
+		if ( $alt ) $tweetcontent = $alt;
+		if ( $hashtag ) $tweetcontent .= " " . $hashtag;
+
+		ob_start();
+			self::template( 'tweet', compact( 'content', 'tweetcontent', 'permalink' ) );
+			$output = ob_get_contents();
+		ob_end_clean();
+		return $output;
+	}
+
+	/** 
+	 * Load a template. MVC FTW!
+	 * @param string $template the template to load, without extension (assumes .php). File should be in templates/ folder
+	 * @param args array of args to be run through extract and passed to template
+	 */
+	public static function template( $template, $args = array() ) {
+
+	    extract( $args );
+
+	    if ( ! $template )
+	        return false;
+
+	    $path = dirname( __FILE__ ) . "/templates/{$template}.php";
+	    $path = apply_filters( 'liveblog', $path, $template );
+
+	    include $path;
+
+	}
 }
 
-add_shortcode( 'tweetable', array('TweetableText', 'makeTweetable') );
+TweetableText::load();
 
-function tweetabletext_header() {
-?>
-<script type="text/javascript">
-jQuery(document).ready(function(){
-  $(".tweetable").hover(
-      function(){
-            if ($(this).data('vis') != true) {
-                    $(this).data('vis', true);
-                    $(this).find('.sharebuttons').fadeIn(200);
-            }
-      },
-      function(){
-            if ($(this).data('vis') === true) {
-                    $(this).find('.sharebuttons').clearQueue().delay(0).fadeOut(200);
-                    $(this).data('vis', false);
-                    $(this).data('leftSet', false);
-            }
-      });
-});	
-</script>
-<style>
-	.tweetable {position: relative;}
-	.tweetable a { text-decoration: none; border-bottom: 0px dotted #ed2e24; color: #333; background: whitesmoke; }
-	.tweetable a:hover { text-decoration: none; border-bottom: 0px dotted #ed2e24; color: #ed2e24;}
-	.sharebuttons {display: none; position: absolute; top: -30px; left: 0px; z-index: 101;  width: 55px; background: #ed2e24; color: whitesmoke; border-radius: 3px; height: 20px; padding: 5px; text-align: center; font-family: 'helvetica neue', helvetica, arial, sans-serif; font-size: 14px; font-weight: bold; }
-	.sharebuttons a { color: whitesmoke; background: #ed2e24; border: 0; }
-	.sharebuttons a:hover { color: whitesmoke; background: #ed2e24; border: 0; }
-	.sharebuttons span.brand a {font-size: 10px; color: whitesmoke; text-decoration:none; display: block; padding: 0 0 0 25px; margin: 0; border: none; height: 12px; }
-</style>
-<?
-}
-
-if (!is_admin()) {
-	add_action(  'wp_head', 'tweetabletext_header' ); 
-}
-?>
+endif;
