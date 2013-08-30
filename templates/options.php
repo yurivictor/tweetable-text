1358f588dc6d3bf499e919c4844e27a6417b73ea
adam schweigert <aschweigert@gmail.com>
2013-08-30 12:30:19 -0400

    first attempt at adding bit.ly url shortening

--- templates/options.php
+++ templates/options.php
@@ -24,7 +24,15 @@
             <tr valign="top">
                 <th scope="row">Twitter username (via @):</th>
                 <td><input type="text" name="<?php echo $key; ?>[username]" value="<?php echo $options['username']; ?>" /></td>
-            </tr>            
+            </tr>
+            <tr valign="top">
+                <th scope="row">Bit.ly username:</th>
+                <td><input type="text" name="<?php echo $key; ?>[bitly_user]" value="<?php echo $options['bitly_user']; ?>" /></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Bit.ly API Key:</th>
+                <td><input type="text" name="<?php echo $key; ?>[bitly_key]" value="<?php echo $options['bitly_key']; ?>" /></td>
+            </tr>
         </table>
         <p class="submit">
             <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
