a0db6fdd7e72ebcb79403704bcbaa95df97738dd
adam schweigert <aschweigert@gmail.com>
2013-08-30 13:51:05 -0400

    this popup bubble gets cut off a lot, making it a little safer

--- templates/css.php
+++ templates/css.php
@@ -3,5 +3,5 @@
 .tweetable a:hover { color: <?php echo $color_hover ?>; text-decoration: none; }
 .tweetable a:hover .share { display: block; }
 .tweetable .icon-twitter { color: <?php echo $color_hover; ?>; }
-.tweetable .share { background: <?php echo $color_hover ?>; border-radius: 4px; color: #fff; display: none; position:absolute; padding: 5px; left: 0; top: -35px; width: 45px; }
+.tweetable .share { background: <?php echo $color_hover ?>; font-family: "helvetica neue", helvetica, sans-serif; font-size: 15px; line-height: 1; border-radius: 4px; -webkit-border-radius: 4px; -moz-border-radius: 4px; color: #fff; display: none; position:absolute; padding: 5px; left: 0; top: -25px; }
 </style>
